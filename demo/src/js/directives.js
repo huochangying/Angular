b35f90e5de04c622d28861c5da662e7ce12ce1f2
unknown <youaremyfuture666@163.com>
2017-03-24 00:48:29 +0800

    2017/3/24

--- demo/src/js/directives.js
+++ demo/src/js/directives.js
@@ -1,16 +1,26 @@
 /**
  * Created by quauq on 2017/3/23.
  */
-var directive=angular.module('directive',[]);
-directive.directive('auto',function () {
+var directive = angular.module('directive', []);
+directive.directive('resize', function ($window) {
     return {
+        scope: {},
         restrict: 'AE',
-        template:'<div id="middle"><ng-transclude></ng-transclude></div>',
-        transclude:true,
-        link:function ($scope,$elem,$attr) {
-            $elem.bind('scroll',function(){
-
-            })
+        controller: function ($scope) {
+            this.resizeUse = function (element) {
+                var _element = angular.element(element);
+                var _winowHeight = $window.innerHeight;
+                var _divHeight = element[0].offsetHeight;
+                var _height = (_winowHeight - _divHeight) / 2;
+                _height > 0 ? _element.css('marginTop', _height + 'px') : null;
+            }
+        },
+        link: function (scope, element, attr, ctrl) {
+            var _window = angular.element($window);
+            ctrl.resizeUse(element);
+            _window.on('resize', function () {
+                ctrl.resizeUse(element);
+            });
         }
     };
 });
